aaea176d838ecd8f7405346ade61fc067d350008
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-03-07 10:37:00 +0400

    Добавлен файл для конвертации текста

--- venv/Cryptology/Encryption/Perestanovka2.py
+++ venv/Cryptology/Encryption/Perestanovka2.py
@@ -1,6 +1,6 @@
 import random
 
-abc = list(map(chr(i) for i in range(ord('А'), ord('Я') + 1)))  # Наш используемый Алфавит - заглавные русские буквы
+abc = list(map(chr, range(ord('А'), ord('Я') + 1)))  # Наш используемый Алфавит - заглавные русские буквы
 key = abc.copy()
 random.shuffle(key)
 enc = dict(zip(abc, key))
--- venv/Cryptology/Trainings/Convert Text.py
+++ venv/Cryptology/Trainings/Convert Text.py
@@ -1,5 +1,16 @@
+abc = frozenset(map(chr, range(ord('А'), ord('Я') + 1)))  # Наш используемый Алфавит - заглавные русские буквы
+
 with open('text1.txt') as f:
     data = f.read()
 print(type(data), len(data))
+data2 = []
+
 
 def convertText(text):
+    for s in data:
+        s = s.upper()
+        if s == 'Ё':
+            s = 'Е'
+        if s in abc:
+            data2.append(s)
+    print(''.join(data2))
