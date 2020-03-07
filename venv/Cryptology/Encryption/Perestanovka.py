ac8b6c8e8729e089bbf3bf7cf304d1bccc01b866
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-03-07 09:34:15 +0400

    Реализована функция шифрования простой перестановкой

--- venv/Cryptology/Encryption/Perestanovka.py
+++ venv/Cryptology/Encryption/Perestanovka.py
@@ -3,7 +3,19 @@ import random
 abc = [chr(i) for i in range(ord('А'), ord('Я') + 1)]  # Наш используемый Алфавит - заглавные русские буквы
 key = abc.copy()
 random.shuffle(key)
-enc = dict(zip(abc,key))
-dec = dict(zip(key,abc))
+enc = dict(zip(abc, key))
+dec = dict(zip(key, abc))
 print(enc)
 print(dec)
+
+
+def convert(d, s):
+    return ''.join(map(lambda t: d[t], s))
+
+
+t1 = 'КРИПТОГРАФИЯ'
+t2 = convert(enc, t1)
+t3 = convert(dec, t2)
+print(t1)
+print(t2)
+print(t3)
