e0302b497b0f96c2ceb617f71c9167598defcd65
Kozikov Dmitriy <kozikov.dmitrii@mail.ru>
2020-03-07 08:45:22 +0400

    Начало перестановки

---
+++ venv/Cryptology/Encryption/Perestanovka.py
@@ -0,0 +1,9 @@
+import random
+
+abc = [chr(i) for i in range(ord('А'), ord('Я') + 1)]  # Наш используемый Алфавит - заглавные русские буквы
+key = abc.copy()
+random.shuffle(key)
+enc = dict(zip(abc,key))
+dec = dict(zip(key,abc))
+print(enc)
+print(dec)
